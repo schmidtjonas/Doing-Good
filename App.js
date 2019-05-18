d7547e34f3ae609c30fd4301c97ffa58e2391f94
rohansaw <rohan.sawahn@student.hpi.de>
2019-05-18 14:04:04 +0200

    login working

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import * as firebase from 'firebase';
@@ -27,29 +19,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
         <AppNavigator/>
-      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
