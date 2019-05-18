e4028ba4dfe3a0397017494d237ab282c44819bd
Jonas Schmidt <mail@schmidt-jonas.de>
2019-05-18 12:37:34 +0200

    added firebase

--- App.js
+++ App.js
@@ -8,22 +8,38 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import * as firebase from 'firebase';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyBM_uen5b9n1ho0YwKILCTHORJHXeScVQA",
+  authDomain: "doinggood.firebaseapp.com",
+  databaseURL: "https://doinggood.firebaseio.com",
+  projectId: "doinggood",
+  storageBucket: "doinggood.appspot.com",
+  messagingSenderId: "787451568515",
+  appId: "1:787451568515:web:765a3a2d4773f99d"
+};
+firebase.initializeApp(firebaseConfig);
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyBM_uen5b9n1ho0YwKILCTHORJHXeScVQA",
+      authDomain: "doinggood.firebaseapp.com",
+      databaseURL: "https://doinggood.firebaseio.com",
+      projectId: "doinggood",
+      storageBucket: "doinggood.appspot.com",
+      messagingSenderId: "787451568515",
+      appId: "1:787451568515:web:765a3a2d4773f99d"
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
