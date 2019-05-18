2e06394f1f4059dbb630cfb75c267fd0811f7022
linaScience <m>
2019-05-18 15:56:22 +0200

    added Colors.js

---
+++ Colors.js
@@ -0,0 +1,20 @@
+const mainFontColor = stormcloud,
+const lightFontColor = beige,
+
+export default {
+  mainFontColor,
+  lightFontColor,
+  tabIconSelected: weldonBlueClicked,
+  buttonColor: weldonBlue,
+  tabBar: weldonBlue,
+  noticeBackground: columbiaBlue,
+  loginBackground: columbiaBlue,
+  noticeText: mainFontColor,
+
+  sunsetOrange : '#fe5ff',
+  beige: '#eef5db',
+  stormcloud: '#4f6367',
+  weldonBlue: '#7a9e9f',
+  weldonBlueClicked: '#8fb3b4',
+  columbiaBlue: '#b8d8d8',
+};
