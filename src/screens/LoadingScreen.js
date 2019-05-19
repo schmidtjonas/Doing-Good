2994eb3edf9708e0378f45c34ee1ba753b31c962
linaScience <m>
2019-05-19 02:30:21 +0200

    Loading screen created but not useable.

---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, Text} from 'react-native';
+import AnimatedLoader from 'react-native-animated-loader';
+
+
+/*export default class App extends React.Component{
+    render(){
+        const{ visible } = this.state;
+        return(
+            <AnimatedLoader
+                visible = {visible}
+                overlayColor="rgba(255, 255, 255, 0.75"
+                source={}
+        );
+    */
