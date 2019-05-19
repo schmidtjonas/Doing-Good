f4575821608b924a594d967bd3aa70ecd7c6217a
linaScience <m>
2019-05-19 03:00:39 +0200

    added icons thumbs_up and thumbs_down

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Button, Text} from 'react-native';
 import AnimatedLoader from 'react-native-animated-loader';
+import LottieView from 'lottie-react-native';
 
+export default class Loader extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { visible : false}
+    }
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                visible: !this.state.visible
+            });
+        }, 30000)
+    }
 
-/*export default class App extends React.Component{
     render(){
         const{ visible } = this.state;
         return(
             <AnimatedLoader
                 visible = {visible}
                 overlayColor="rgba(255, 255, 255, 0.75"
-                source={}
+                source={require("")}
+                animationStyle={StyleSheet.lottie}
+                speed={1}
+            />
         );
-    */
+    }
+}
+
+const styles= StyleSheet.create({
+    lottie:{
+        width:100,
+        height: 100
+    }
+});
